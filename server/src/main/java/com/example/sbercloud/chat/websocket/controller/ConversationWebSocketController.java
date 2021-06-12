7e53f59c5fedd39000e3087c931079300fa11858
Denis Bulygin <Bulygin.D.Nik@sberbank.ru>
2021-06-12 14:53:55 +0300

    убрал создание беседы через вебсокет

--- server/src/main/java/com/example/sbercloud/chat/websocket/controller/ConversationWebSocketController.java
+++ server/src/main/java/com/example/sbercloud/chat/websocket/controller/ConversationWebSocketController.java
@@ -1,7 +1,6 @@
 package com.example.sbercloud.chat.websocket.controller;
 
 import com.example.sbercloud.chat.dal.entity.ConversationEntity;
-import com.example.sbercloud.chat.dal.repository.ConversationRepository;
 import com.example.sbercloud.chat.model.Conversation;
 import com.example.sbercloud.chat.model.Message;
 import lombok.RequiredArgsConstructor;
@@ -9,13 +8,7 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.stereotype.Controller;
-
-import java.util.List;
-import java.util.stream.StreamSupport;
-
-import static java.util.stream.Collectors.toList;
 
 /**
  * @author Bulygin D.N.
@@ -27,28 +20,11 @@ public class ConversationWebSocketController {
 
     private final SimpMessagingTemplate messagingTemplate;
 
-    private final ConversationRepository conversationRepository;
-
-    @SubscribeMapping("/conversations")
-    public List<Conversation> retrieveParticipants() {
-        Iterable<ConversationEntity> conversationEntities = conversationRepository.findAll();
-        List<Conversation> conversations = StreamSupport.stream(conversationEntities.spliterator(), false)
-                .map(this::mapConversationEntityToConversation)
-                .collect(toList());
-        return conversations;
-    }
-
     @MessageMapping("/conversations/{conversationId}")
     public void addMessageToConversation(@Payload Message message, @DestinationVariable("conversationId") String conversationId) {
         // path example: /conversations/1/messages
         messagingTemplate.convertAndSend("/conversations/" + conversationId + "/messages", message);
     }
-
-//    @MessageMapping("/conversations/{conversationId}")
-//    public void addMessageToConversation(@Payload Message message, @DestinationVariable("conversationId") String conversationId) {
-//        // path example: /conversations/1/messages
-//        messagingTemplate.convertAndSend("/conversations/" + conversationId + "/messages", message);
-//    }
 
     private Conversation mapConversationEntityToConversation(ConversationEntity conversationEntity) {
         Conversation conversation = new Conversation();
