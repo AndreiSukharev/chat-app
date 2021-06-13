47fa8e796f929ccbc766910c79eee286a5aaadb4
Pavel Sklyarov <sklyarovpavel@gmail.com>
2021-06-13 10:49:54 +0300

    workin copy v1

--- server/src/main/java/com/example/sbercloud/chat/SberCloudChatApplication.java
+++ server/src/main/java/com/example/sbercloud/chat/SberCloudChatApplication.java
@@ -19,9 +19,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -30,6 +27,21 @@ public class SberCloudChatApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SberCloudChatApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry
+						.addMapping("/**")
+						.allowCredentials(true)
+						.allowedHeaders("*")
+						.allowedOrigins("*")
+						.allowedMethods("*");
+			}
+		};
 	}
 
 }
