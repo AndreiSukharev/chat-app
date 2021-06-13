4263624ecff67db5358e156fe16cc0a84e1c1e99
vladisslaww <vladisslaww@gmail.com>
2021-06-13 09:29:31 +0300

    client: switched to stomp

--- client/src/lib/ChatService.ts
+++ client/src/lib/ChatService.ts
@@ -2,17 +2,16 @@ import {
   ChatEvent,
   ChatEventHandler,
   ChatEventType,
-  ChatMessage,
   IChatService,
   IStorage,
-  MessageContentType,
-  MessageDirection,
-  MessageEvent,
   SendMessageServiceParams,
   SendTypingServiceParams,
   UpdateState,
-  UserTypingEvent,
+  MessageEvent,
 } from '@chatscope/use-chat';
+import SockJS from 'sockjs-client';
+import { SOCK_HOST } from '../env';
+import { Stomp } from '@stomp/stompjs';
 
 type EventHandlers = {
   onMessage: ChatEventHandler<ChatEventType.Message, ChatEvent<ChatEventType.Message>>;
@@ -30,9 +29,23 @@ type EventHandlers = {
   [key: string]: any;
 };
 
+const subscriptions = new Map();
+const topics = ['/conversations/messages'];
+const client = Stomp.over(new SockJS(SOCK_HOST, null));
+
+const processMessage = (msgBody: any) => {
+  try {
+    return JSON.parse(msgBody);
+  } catch (e) {
+    return msgBody;
+  }
+};
+
 export class ChatService implements IChatService {
   storage?: IStorage;
   updateState: UpdateState;
+  retryCount: number = 0;
+  timeoutId?: ReturnType<typeof setTimeout>;
 
   eventHandlers: EventHandlers = {
     onMessage: () => {},
@@ -46,75 +59,32 @@ export class ChatService implements IChatService {
   constructor(storage: IStorage, update: UpdateState) {
     this.storage = storage;
     this.updateState = update;
+    client.heartbeat.outgoing = 10000;
+    client.heartbeat.incoming = 10000;
 
-    // For communication we use CustomEvent dispatched to the window object.
-    // It allows you to simulate sending and receiving data from the server.
-    // In a real application, instead of adding a listener to the window,
-    // you will implement here receiving data from your chat server.
-    window.addEventListener('chat-protocol', (evt: Event) => {
-      const event = evt as CustomEvent;
-
-      const {
-        detail: { type },
-        detail,
-      } = event;
-
-      if (type === 'message') {
-        const message = detail.message as ChatMessage<MessageContentType.TextHtml>;
-
-        message.direction = MessageDirection.Incoming;
-        const { conversationId } = detail;
-        if (this.eventHandlers.onMessage && detail.sender !== this) {
-          // Running the onMessage callback registered by ChatProvider will cause:
-          // 1. Add a message to the conversation to which the message was sent
-          // 2. If a conversation with the given id exists and is not active,
-          //    its unreadCounter will be incremented
-          // 3. Remove information about the sender who is writing from the conversation
-          // 4. Re-render
-          //
-          // Note!
-          // If a conversation with such id does not exist,
-          // the message will be added, but the conversation object will not be created.
-          // You have to take care of such a case yourself.
-          // You can check here if there is already a conversation in storage.
-          // If it is not there, you can create it before calling onMessage.
-          // After adding a conversation to the list, you don't need to manually run updateState
-          // because ChatProvider in onMessage will do it.
-          this.eventHandlers.onMessage(new MessageEvent({ message, conversationId }));
+    client.connect(
+      {},
+      () => {
+        topics.forEach((topic) => {
+          this.subscribe(topic);
+        });
+      },
+      (error: any) => {
+        if (error) {
         }
-      } else if (type === 'typing') {
-        const { userId, isTyping, conversationId, content, sender } = detail;
+      },
+    );
+  }
 
-        if (this.eventHandlers.onUserTyping && sender !== this) {
-          // TODO: Przetłumaczyć
-          // Uruchomienie callbacka onUserTyping zarejestrowanego przez ChatProvider spowoduje
-          // 1. Dodanie użytkownika do listy piszących w danej konwersacji
-          // 2. Debounce
-          // 3. Przerenderowanie
-          this.eventHandlers.onUserTyping(
-            new UserTypingEvent({
-              userId,
-              isTyping,
-              conversationId,
-              content,
-            }),
-          );
-        }
-      }
-    });
+  setStorage(storage: IStorage) {
+    this.storage = storage;
+  }
+  setUpdateState(update: UpdateState) {
+    this.updateState = update;
   }
 
   sendMessage({ message, conversationId }: SendMessageServiceParams) {
-    const messageEvent = new CustomEvent('chat-protocol', {
-      detail: {
-        type: 'message',
-        message,
-        conversationId,
-        sender: this,
-      },
-    });
-
-    window.dispatchEvent(messageEvent);
+    this.send('/app/conversations', JSON.stringify({ ...message, conversationId }));
 
     return message;
   }
@@ -148,4 +118,37 @@ export class ChatService implements IChatService {
       this.eventHandlers[key] = () => {};
     }
   }
+
+  send(topic: string, msg: any, optHeaders?: any) {
+    client.send(topic, optHeaders, msg);
+  }
+
+  subscribe = (topic: string) => {
+    if (!subscriptions.has(topic)) {
+      const self = this;
+      let sub = client.subscribe(
+        topic,
+        (msg) => {
+          const message = processMessage(msg.body);
+          console.log(message);
+          self.eventHandlers.onMessage(new MessageEvent({ message, conversationId: message.conversationId }));
+        },
+        {},
+      );
+      subscriptions.set(topic, sub);
+    }
+  };
 }
+
+let Service: ChatService;
+
+export const serviceFactory = (storage: IStorage, updateState: UpdateState) => {
+  if (!Service) {
+    Service = new ChatService(storage, updateState);
+    return Service;
+  }
+  Service.setStorage(storage);
+  Service.setUpdateState(updateState);
+
+  return Service;
+};
